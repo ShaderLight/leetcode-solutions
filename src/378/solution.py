3506c66627d504c46a38291a2e30c041d36703b0
ShaderLight <37264318+ShaderLight@users.noreply.github.com>
2022-08-02 22:46:34 +0200

    378 update

--- src/378/solution.py
+++ src/378/solution.py
@@ -1,5 +1,5 @@
-from typing import*
-import bisect
+from typing import List
+from bisect import bisect_right
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
@@ -8,7 +8,7 @@ class Solution:
         while min_val < max_val:
             mid_val = self.halfUtil(min_val, max_val)
 
-            smaller_count = self.howManySmaller(matrix, mid_val)
+            smaller_count = self.kSmaller(matrix, mid_val)
 
             if smaller_count < k:
                 min_val = mid_val + 1
@@ -17,16 +17,19 @@ class Solution:
 
         return min_val
 
-    def howManySmaller(self, matrix, val):
+    def kSmaller(self, matrix: List[List[int]], val: int) -> int:
         count = 0
         for row in matrix:
-            count += bisect.bisect_right(row, val)
+            count += bisect_right(row, val)
 
         return count
 
-    def halfUtil(self, s, e):
+    def halfUtil(self, s: int, e: int) -> int:
         return (s+e)//2
 
-obj = Solution()
 
-print(obj.kthSmallest([[1,5,9],[10,11,13],[12,13,15]], 8))
+if __name__=='__main__':
+    # Example driver code
+    obj = Solution()
+    print(obj.kthSmallest([[1,5,9],[10,11,13],[12,13,15]], 8)) 
+    # Should yield 13
